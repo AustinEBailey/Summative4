8d479fb0151215244723dba930dfb5fa96c06198
AustinEBailey <79767912+AustinEBailey@users.noreply.github.com>
2023-05-27 09:15:06 -0400

    Update MiniGame.java
    made it so coins did not print in the same place twice.

--- MiniGame.java
+++ MiniGame.java
@@ -11,27 +11,27 @@ public class MiniGame {
         String[] coins = { "0", "o" };
 
         String sprite = "!";
-        int pCollum = 0;
-        int pRow = 0;
+        int playerCollum = 0;
+        int playerRow = 0;
 
         boolean done = false;
         while (!done) {
-
+            int row = 0;
+            int collum = 0;
             prn("\033[H\033[2J");
-            prnMap(map, pCollum, sprite, pRow, coins);
+            prnMap(map, playerCollum, sprite, playerRow, coins);
             String input = in.nextLine();
-
-            if (input.equals("l")) {
-                pCollum--;
-            } else if (input.equals("r")) {
-                pCollum++;
-            } else if (input.equals("c")) {
-                coin(map, pCollum);
+            String[] inputWords = input.split(",");
+            if(inputWords[0].equals("collum")) {
+                playerCollum  = Integer.parseInt(inputWords[1]);
+            }else if (inputWords[0].equals("coin")) {
+                insertCion(map, playerCollum, playerRow);
             }
 
         }
     }
 
+    // dose the initialization of the map
     public static int[][] loadMap() {
         int[][] map = {
                 { 0, 0, 0, 0, 0, 0, 0 },
@@ -46,10 +46,10 @@ public class MiniGame {
     }
 
     // prints the map.
-    public static void prnMap(int[][] map, int pCollum, String sprite, int pRow, String[] coins) {
+    public static void prnMap(int[][] map, int playerCollum, String sprite, int playerRow, String[] coins) {
         for (int i = 0; i < map.length; i++) {
             for (int j = 0; j < map[i].length; j++) {
-                if ((i == pRow && j == pCollum)) {
+                if ((i == playerRow && j == playerCollum)) {
                     prn(sprite);
                 } else {
                     int prnMap = map[i][j];
@@ -60,9 +60,15 @@ public class MiniGame {
         }
     }
 
-    public static int[][] coin(int[][] map, int pCollum) {
-        map[3][pCollum] = 1;
-        return map;
+    // prints coins where ever the sprite is when "c" is entered
+    public static void insertCion(int[][] map, int playerCollum, int playerRow) {
+
+        if (map[playerRow][playerCollum] != 0) {
+            // it dose nothing exep prevent coins from being placed in the same place.
+        } else {
+            map[playerRow][playerCollum] = 1;
+        }
+
     }
 
     // print stuff
